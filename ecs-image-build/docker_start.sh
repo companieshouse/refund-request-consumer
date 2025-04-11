a46d6c2c7ba1bd107b5f141009f97be2104c19db
rhilgenbergbastos-ch <rhilgenbergbastos@companieshouse.gov.uk>
2025-04-11 12:27:06 +0100

    feat: add docker and tf changes

---
+++ ecs-image-build/docker_start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Start script for refund-request-consumer
+
+APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Read brokers from environment and split on comma
+IFS=',' read -a BROKERS <<< "${KAFKA_BROKER_ADDR}"
+
+# Ensure we only populate the broker address via application arguments
+unset KAFKA_BROKER_ADDR
+
+exec "${APP_DIR}/refund-request-consumer" $(for b in "${BROKERS[@]}"; do echo -n "-broker-addr=${b} "; done)
