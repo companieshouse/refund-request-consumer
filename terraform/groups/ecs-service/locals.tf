c9e20098873b783b8e926f826c96cfb2c678c573
rhilgenbergbastos-ch <rhilgenbergbastos@companieshouse.gov.uk>
2025-04-11 12:33:17 +0100

    fix: update stack name

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -1,6 +1,6 @@
 # Define all hardcoded local variable and local variables looked up from data resources
 locals {
-  stack_name                 = "payments" # this must match the stack name the service deploys into
+  stack_name                 = "payments-service" # this must match the stack name the service deploys into
   name_prefix                = "${local.stack_name}-${var.environment}"
   global_prefix              = "global-${var.environment}"
   service_name               = "refund-request-consumer"
