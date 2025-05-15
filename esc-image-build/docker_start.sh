140b90b5e0503eefcd17c794d3382c8290649c67
rhilgenbergbastos-ch <rhilgenbergbastos@companieshouse.gov.uk>
2025-05-15 13:50:50 +0100

    feat: remove esc folder

--- esc-image-build/docker_start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-# Start script for refund-request-consumer
-APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-exec "${APP_DIR}/refund-request-consumer" "-bind-addr=:${PORT}"
